9a51c3fab75431adacdece1d01c1c0e5af9dabbd
DrPandemic <bipbip500@gmail.com>
2017-10-21 14:39:00 -0400

    Updates dockerfile

--- deploy.js
+++ deploy.js
@@ -1,3 +1,3 @@
 const { execSync } = require('child_process');
 
-console.log(execSync(`docker exec twioliobackend_functions_1 functions-emulator deploy ${process.argv[2]} --trigger-http --local-path /src/app/`).toString());
+console.log(execSync(`docker exec twioliobackend_functions_1 functions-emulator deploy ${process.argv[2]} --trigger-http --local-path /usr/src/app/`).toString());
